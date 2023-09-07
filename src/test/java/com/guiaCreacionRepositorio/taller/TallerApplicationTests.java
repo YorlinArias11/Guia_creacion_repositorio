91c467bd15e0973033ab3a7fa443ae9fd602b14a
Yorlin Arias <yorlinandrey@gmail.com>
2023-09-07 18:13:38 -0500

    Se agregó un nuevo proyecto github usando springboot

---
+++ src/test/java/com/guiaCreacionRepositorio/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiaCreacionRepositorio.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
